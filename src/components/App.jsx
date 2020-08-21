355c7e475f03885cdf8d97dca3b7e544c7cfb7ba
matchashiba <recr38@gmail.com>
2020-08-21 22:43:24 +0000

    Defined function getShibes in App Inserted fetch request to shibe.online API Added constructor to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pictures: null
+    };
+  }
+
+  getShibes(quantity) {
+    const url = `http://shibe.online/api/shibes?count=${quantity}&urls=true&httpsUrls=true`;
+    fetch(url)
+      .then(data => data.json())
+      .then(shibes => {
+        this.setState({ pictures: shibes })
+      .catch(err => console.error(`Error: ${err}`))
+      });
+  }
+
   render() {
     return (
     <>
