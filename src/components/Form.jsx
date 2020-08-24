79898937721f11b4cb10e5067adcece7cfbb8898
matchashiba <recr38@gmail.com>
2020-08-24 20:29:24 +0000

    Created initial Form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="form-inline">Form</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Form;
